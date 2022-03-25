3fa4ba188fb7cc709547cf30eb02ae2650386f97
Rachel <racheldgalang@gmail.com>
2022-03-25 17:48:19 -0400

    complete challenge vii "Create a second button and input box that will add the text supplied in the input box into the function created in Challenge IV when the button is clicked."

--- challenge_iv-vii/script.js
+++ challenge_iv-vii/script.js
@@ -1,13 +1,22 @@
 let listItems = new Array();
 let listIndex = 0;
 
-function addToList() {
+function addItemFromPrompt() {
     let newItem = prompt("Please enter new item", "");
+    updateList(newItem);
+}
+
+function addItemFromForm() {
+    let newItem = document.getElementById("itemInput").value;
+    updateList(newItem);
+}
+
+function updateList(newItem) {
     listItems[listIndex] = newItem;
     listIndex++;
-    let item = document.createElement('li');
-    item.appendChild(document.createTextNode(newItem));
 
+    let item = document.createElement('li');
+    item.appendChild(document.createTextNode(newItem)); 
     document.querySelector('ul').appendChild(item);
 }
 
