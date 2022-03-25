c56858999c82da4689872f1689c6e290b6a4fbd6
Rachel <racheldgalang@gmail.com>
2022-03-25 15:23:21 -0400

    complete challenge v "Create a function that will print the list of items in an alert."

--- challenge_iv-vii/script.js
+++ challenge_iv-vii/script.js
@@ -1,10 +1,16 @@
-let x = document.getElementById("addBtn");
+let listItems = new Array();
+let listIndex = 0;
 
 function addToList() {
     let newItem = prompt("Please enter new item", "");
-    
+    listItems[listIndex] = newItem;
+    listIndex++;
     let item = document.createElement('li');
     item.appendChild(document.createTextNode(newItem));
 
     document.querySelector('ul').appendChild(item);
+}
+
+function printListToAlert() {
+    alert(listItems.toString());
 }
